00060d0e4c840bfb6ff2eb0ff267a821b5349718
KhanyisoHaman <khanyisohaman@gmail.com>
2019-04-09 14:32:59 +0200

    add content

---
+++ js/main.js
@@ -0,0 +1,12 @@
+
+// JQUERY
+// For my back to top button
+var btn = $('#button');
+
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 300) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
+});
